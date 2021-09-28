f04c5237d3814eb6af879ca81b7f81d5b0224d0c
GLjuben <gorjanljuben@gmail.com>
2021-09-28 14:37:11 -0700

    fizzbuzz solved

--- Cardio_1/app.js
+++ Cardio_1/app.js
@@ -240,14 +240,44 @@ function capitalizeLetter(str){
 // Return the character that is most common in a string 
 // ex. maxCharacter('javascript )=== 'a';
 function maxCharacter(str) {
-  
-  //with regex:
-
-
-   //  New comment!
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+  str.split('').forEach(char => {
+    if (charMap[char]){
+      charMap[char]++
+    } else {
+      charMap[char]=1;
+    }
+  });
+  // loop throughout the object and find the char with max reps;
+  for (let char in charMap) {
+    if(charMap[char]>maxNum){
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
-const outPut = capitalizeLetter('i love javascript');
-console.log(outPut)
+
+//Challenge 6: fizzbuzz;
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3,instead of the number, print "Fizz",for the multiples of 5 print "Buzz".
+// For numbers which are multiple of 3 and 5, print "FizzBuzz".
+function fizzBuzz() {
+ for(let i = 1; i<=100; i++){
+    if(i % 3 === 0 && i % 5 === 0 ){
+      console.log('FizzBuzz')
+    }else if(i%3===0){
+     console.log('Fizz');
+   } else if(i%5===0){
+      console.log('Buzz')
+   } else {
+     console.log(i);
+   }
+ }
+}
+const outPut = fizzBuzz();
+// console.log(outPut)
   
   
   
